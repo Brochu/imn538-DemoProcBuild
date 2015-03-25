da886d52bd78d4014b1c28dadba812dacde62547
Brochu <alexbrochu1@gmail.com>
2015-03-24 21:03:15 -0400

    Added basic definition for the [shape] grammar engine

---
+++ DemoProcBuild/GrammarEngine.h
@@ -0,0 +1,24 @@
+#ifndef _GRAMMARENGINE_H_
+#define _GRAMMARENGINE_H_
+
+#include "ShapeGrammar.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class GrammarEngine
+{
+public:
+    GrammarEngine();
+    GrammarEngine(ShapeGrammar* sg);
+    ~GrammarEngine();
+
+    void debug();
+
+private:
+    ShapeGrammar* shapeGrammar; // To delete in destructor !
+
+};
+
+#endif
