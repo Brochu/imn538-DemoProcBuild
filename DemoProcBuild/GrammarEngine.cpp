18d66eeb6b9f316577bdf2daa35e5d2dfcb99cd9
Brochu <alexbrochu1@gmail.com>
2015-03-29 04:30:07 -0400

    added some utility functions to use the lists of shapes

--- DemoProcBuild/GrammarEngine.cpp
+++ DemoProcBuild/GrammarEngine.cpp
@@ -13,10 +13,37 @@ GrammarEngine::~GrammarEngine()
 
 void GrammarEngine::debug()
 {
-    // Print some information about the engine, state, to see if everything is working fine.
+    cout << "Information about GrammarEngine class: " << endl;
+    cout << "\t nb of terminal shapes : " << terminals.size() << endl;
+    cout << "\t nb of non-terminal shapes : " << nonTerminals.size() << endl;
+}
+
+void GrammarEngine::addShape(glm::mat4& transValues, ShapeState state, Shape s)
+{
+    vector<ShapeInfo>* toadd = 0;
+
+    if (state == GrammarEngine::ShapeState::terminal)
+        toadd = &terminals;
+    else if (state == GrammarEngine::ShapeState::nonTerminal)
+        toadd = &nonTerminals;
+
+    ShapeInfo si;
+    si.transInfo = transValues;
+    si.type = s;
+
+    toadd->insert(toadd->end(), si);
 }
 
 void GrammarEngine::drawShapes()
 {
-    // Cycles through terminal and non terminal shapes
+    vector<ShapeInfo>::iterator i;
+    for (i = nonTerminals.begin(); i != nonTerminals.end(); ++i)
+    {
+        // draw shape
+    }
+
+    for (i = terminals.begin(); i != terminals.end(); ++i)
+    {
+        // draw shape
+    }
 }
--- DemoProcBuild/GrammarEngine.h
+++ DemoProcBuild/GrammarEngine.h
@@ -2,6 +2,7 @@
 #define _GRAMMARENGINE_H_
 
 #include "ShapeGrammar.h"
+#include "glm\glm.hpp"
 #include <string>
 #include <iostream>
 #include <vector>
@@ -15,11 +16,31 @@ public:
     GrammarEngine(ShapeGrammar* sg);
     ~GrammarEngine();
 
+    enum ShapeState{
+        terminal = 0,
+        nonTerminal = 1
+    };
+
+    enum Shape{
+        cube = 0,
+        cylinder = 1,
+        sphere = 2
+    };
+
     void debug();
+    void addShape(glm::mat4&, ShapeState, Shape);
     void drawShapes();
 
 private:
     ShapeGrammar* shapeGrammar;
+
+    struct ShapeInfo{
+        glm::mat4 transInfo;
+        Shape type;
+        // 0 = cube;
+        // 1 = cylinder
+        // 2 = sphere
+    };
     vector<ShapeInfo> nonTerminals;
     vector<ShapeInfo> terminals;
 
